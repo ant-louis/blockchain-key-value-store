a8a0e45ed4de969e61c2c62c5d5ccb4fe726f35f
Lambo97 <lamborellemaxime@MacBookMaxime.local>
2018-12-18 18:40:37 +0100

    Update of node.py

--- code/keychain/node.py
+++ code/keychain/node.py
@@ -1,24 +1,68 @@
 from flask import Flask, request
 from blockchain import Blockchain
 import json
-import requests
+from requests import get
 
 class Node:
 
     def __init__(self, difficulty=3):
         self.blockchain = Blockchain(difficulty)
         self.peers = []
+        self.ip = get('https://api.ipify.org').text
 
     def get_chain(self):
         return self.blockchain
     
-    def broadcast_bootstrap(self, peers):
-        pass
+    def bootstrap(self, boostrap_adress):
 
-    def broadcast(self, peers, message):
-        pass
+        url = "http://{}/peers".format(boostrap_adress)
+        result = get(url)
+        if result.status_code != 200:
+            print("unable to connect the bootstrap server")
+            return
+        peers = result.json()["peers"]
+        for peer in peers:
+            self.add_node(peer)
+        
+        results = self.broadcast(self.peers, self.ip, "addNode")
+        
+        
+
+
+        
+        
+
+
+    def get_last_hash(self):
+        return self.blockchain.get_last_hash()
+
+    def broadcast(self, peers, message, message_type):
+        """ 
+        Best effort broadcast
+        """
+        results = {}
+        for peer in peers:
+            url = "http://{}/message".format(peer.get_adress())
+            results[peer.get_adress()] = get(url, data=json.dumps({"type": message_type, "message": message})).json()
+        
+        return results
     
+    def add_node(self, peer):
+        new_peer = Peer(peer)
+        self.peers.append(new_peer)
 
+    def get_ip(self):
+        return self.ip
+
+class Peer:
+    def __init__(self, address):
+        """Address of the peer.
+
+        Can be extended if desired.
+        """
+        self._address = address
+    def get_adress(self):
+        return self._address
         
 
 app = Flask(__name__)
@@ -38,16 +82,27 @@ def get_chain():
 @app.route("/bootstrap", methods=['POST'])
 def boostrap():
     boostrap_adress = request.get_json()["bootstrap"]
-    url = "http://{}/peers".format(boostrap_adress)
-    result = request.get(url)
-    if result.status_code != 200:
-        print("unable to connect the bootstrap server")
-        return
-    peers = result.json()["peers"]
-    
+    node.bootstrap(boostrap_adress)
 
 
-    
+@app.route("/addNode", methods=['POST'])
+def add_node():
+    adress = request.get_json()["adress"]
+    node.add_node(adress)
+    return json.dumps({"last_hash": node.get_last_hash()})
+
+
+@app.route("/message", methods=['POST'])
+def message_hanler():
+    message_type = request.get_json()["message_type"]
+    message = request.get_json()["message"]
+
+    if(message_type == "addNode"):
+        node.add_node(message)
+    elif():
+        pass
+    else:
+        pass
 
 app.run(debug=True, port=5000)
 
