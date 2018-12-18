0902f6c429c4b342cc9e2f1e16b3013b90386ad9
“Antoine <antoine.louis02@outlook.fr>
2018-12-18 18:44:36 +0100

    Get request

--- code/keychain/store.py
+++ code/keychain/store.py
@@ -1,34 +1,26 @@
-"""
-KeyChain key-value store (stub).
-
-NB: Feel free to extend or modify.
-"""
-
 from keychain import Blockchain
 from keychain import Transaction
+import node.py
 
 
-class Callback:
-    def __init__(self, transaction, chain):
-        self._transaction = transaction
-        self._chain = chain
+# class Callback:
+#     def __init__(self, transaction, chain):
+#         self._transaction = transaction
+#         self._chain = chain
 
-    def wait(self):
-        """Wait until the transaction appears in the blockchain."""
-        raise NotImplementedError
+#     def wait(self):
+#         """Wait until the transaction appears in the blockchain."""
+#         raise NotImplementedError
 
-    def completed(self):
-        """Polls the blockchain to check if the data is available."""
-        raise NotImplementedError
+#     def completed(self):
+#         """Polls the blockchain to check if the data is available."""
+#         raise NotImplementedError
 
 
 class Storage:
-    def __init__(self, bootstrap, miner, difficulty):
-        """Allocate the backend storage of the high level API, i.e.,
-        your blockchain. Depending whether or not the miner flag has
-        been specified, you should allocate the mining process.
-        """
-        self._blockchain = Blockchain(bootstrap, difficulty)
+    def __init__(self, bootstrap, miner):
+        if miner:
+
         
 
     def put(self, key, value, block=True):
@@ -37,14 +29,45 @@ class Storage:
         The block flag indicates whether the call should block until the value
         has been put onto the blockchain, or if an error occurred.
         """
+        url = "http://{}/put".format(key, value)
+        result = request.get(url)
+        if result.status_code != 200:
+            print("unable to connect the bootstrap server")
+            return
     
-        transaction = Transaction(key, value, id)
-        self._blockchain.add_transaction(self, transaction)
-        callback = Callback(transaction, self._blockchain)
-        if block:
-            callback.wait()
+        
+        # callback = Callback(transaction, self._blockchain)
+        # if block:
+        #     callback.wait()
 
-        return callback
+        # return callback
+
+    def retrieve(self, key):
+        """Searches the most recent value of the specified key.
+
+        -> Search the list of blocks in reverse order for the specified key,
+        or implement some indexing schemes if you would like to do something
+        more efficient.
+        """
+        url = "http://{}/retrieve".format(key)
+        result = request.get(url)
+        if result.status_code != 200:
+            print("unable to connect the bootstrap server")
+            return
+
+        
+
+    def retrieve_all(self, key):
+        """Retrieves all values associated with the specified key on the
+        complete blockchain.
+        """
+        url = "http://{}/retrieve_all".format(key)
+        result = request.get(url)
+        if result.status_code != 200:
+            print("unable to connect the bootstrap server")
+            return
+
+
 
     # def retrieve(self, key):
     #     """Searches the most recent value of the specified key.
